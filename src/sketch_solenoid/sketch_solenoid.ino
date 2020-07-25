53f7461c5480458aebc945c8773bcabc6f6e5296
alastair-duncan <ad@alastairduncan.com>
2020-07-25 16:29:16 +0100

    changed to use hall effect pedal

--- src/sketch_solenoid/sketch_solenoid.ino
+++ src/sketch_solenoid/sketch_solenoid.ino
@@ -1,21 +1,22 @@
 /*
- * Arduino Uno sketch to show how it can be used as a speed controller for
- * driving a solenoid
- */
+   Arduino Uno sketch to show how it can be used as a speed controller for
+   driving a solenoid
+*/
 
 
-/* 
- *  this is the pwm digital pin 2 in this example
- */
-#define MOSFET_PIN 2
-/* This is the A0 pin which will be used 
- *  to get the value for the speed
- */
-#define SENSOR_PIN 0
 /*
- * The number of steps for the speed control
- */
-#define QUANTIZATION_FACTOR 100
+    this is the pwm digital pin 2 in this example
+*/
+#define MOSFET_PIN 2
+
+/*
+   sensor pin for the accellerator pedal
+*/
+#define ACCELERATOR_PEDAL  1
+
+int power_buff = -1;
+
+#define QUANTIZATION_FACTOR 200
 void setup() {
   // uncomment if you want to use the serial monitor on your computer
   Serial.begin(115200);
@@ -25,34 +26,40 @@ void setup() {
 }
 
 void loop() {
-/*
- * grab the value from the sensor pin and map it to the quantization factor.
- * In this example there will be 100 different speed values
- * pot_value is the mapped potentiometer value
- */
-  int pot_value = map(analogRead(SENSOR_PIN),0,1023, 1, QUANTIZATION_FACTOR);
- 
- //uncomment if you want to see the values of the potentiometer in the serial monitor
-  Serial.print( pot_value);
-  Serial.print( "\n");
-/*
- * if the mapped potentiometer value is in the range
- * 0 to 99 then write to the 
- */
-  if(pot_value < 100 ){
-   Serial.print( "About to write high");
+  /*
+     grab the value from the sensor pin and map it to the quantization factor.
+     In this example there will be 100 different speed values
+     pot_value is the mapped potentiometer value
+  */
+  
+  // hall effect footpedal values
+  int pot_value = map(analogRead(ACCELERATOR_PEDAL), 180, 875, -1, QUANTIZATION_FACTOR);
+  
+  // uncomment if you want to see the values of the potentiometer in the serial monitor
+  // Serial.print("acc ");
+  // Serial.println( pot_value);
+
+  pot_value = QUANTIZATION_FACTOR - pot_value;
+
+  /*
+     if the mapped potentiometer value is in the range
+     0 to 99 then write to the mosfet pin
+  */
+  
+  if (pot_value < 200 ) {
+    //Serial.print( "About to write high");
     digitalWrite(MOSFET_PIN, HIGH);
     // this is to give the solenoid time to act
-    delay(15);
-    
+    delay(12);
+
     digitalWrite(MOSFET_PIN, LOW);
     //this is the speed control here. it also determins the duty cycle for the solenoid
-    delay(pot_value + 20);
-  }else
+    delay(pot_value + 12);
+  } else
   {
-    // if its out of the 0 - 99 range ensure that the mosfet is low 
+    // if its out of the 0 - 99 range ensure that the mosfet is low
     digitalWrite(MOSFET_PIN, LOW);
-    delay(pot_value + 20);
+    delay(pot_value + 15);
   }
-  
+
 }
